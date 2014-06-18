36adeef260b728cdc8010b3cc4bcb0cac1667bd7
King Z <kingz@relationshipforum.org>
2014-06-18 16:27:16 -0500

    Thank stats

--- app/models/thank.rb
+++ app/models/thank.rb
@@ -1,4 +1,8 @@
 class Thank < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+
+  def self.thank_stats_for(user)
+    Thank.joins(:post).where("posts.author_id = ?", user.id).count
+  end
 end
