fbb823caa2ea6389a6f141265334deacb8113cd2
King Z <kingz@relationshipforum.org>
2014-06-18 20:58:16 -0500

    Country flags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -18,4 +18,12 @@ module PostsHelper
 
     @renderer.render(post.body).html_safe
   end
+
+  def country_flag(country)
+    if !country
+      ""
+    else
+      image_tag("flags/#{country.to_s.downcase}.png")
+    end
+  end
 end
