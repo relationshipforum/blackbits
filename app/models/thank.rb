de3054250c47cd61b5dc8506315dc1c90348a1a9
King Z <kingz@relationshipforum.org>
2014-06-18 20:24:38 -0500

    Better Thank statistics

--- app/models/thank.rb
+++ app/models/thank.rb
@@ -3,6 +3,9 @@ class Thank < ActiveRecord::Base
   belongs_to :post
 
   def self.thank_stats_for(user)
-    Thank.joins(:post).where("posts.author_id = ?", user.id).count
+    query = Thank.joins(:post).where("posts.author_id = ?", user.id)
+    posts = query.select("DISTINCT thanks.post_id")
+
+    "#{query.count} times, #{posts.count} posts"
   end
 end
