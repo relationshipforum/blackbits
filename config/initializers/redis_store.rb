126ffad84cbe7baf77fa0da63a378a3a2314b086
King Z <kingz@relationshipforum.org>
2014-06-20 19:36:21 -0500

    Use local redis on laptop

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,5 +1,10 @@
-REDIS_URL = "redis://:herpityderpityderp!@cnmrelo.lyceed.com:6379/0/bb"
-REDIS     = Redis.new(url: REDIS_URL)
+if Rails.env == "production"
+  REDIS_URL = "redis://:herpityderpityderp!@cnmrelo.lyceed.com:6379/0/bb"
+else
+  REDIS_URL = "redis://127.0.0.1:6379/0/bb"
+end
+
+REDIS = Redis.new(url: REDIS_URL)
 
 Blackbits::Application.config.cache_store = :redis_store, REDIS_URL
 Blackbits::Application.config.session_store :redis_store, redis_server: REDIS_URL
