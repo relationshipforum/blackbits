101bd255df2aae3dbc97f118b6ebf456e9b00bce
King Z <kingz@relationshipforum.org>
2014-06-23 19:17:00 -0500

    momentjs fix for safari

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -15,7 +15,7 @@ var moment = window.moment || {};
     function onMessage(e) {
         var data = JSON.parse(e.data),
             panel = $("#chat .panel-body"),
-            time = moment(data.timestamp),
+            time = moment(data.timestamp.replace(" ", "T").substring(0, 19)),
             template;
 
         template = " " +
