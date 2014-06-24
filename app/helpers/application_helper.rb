384919770a15e7e05bfa0c8c1b3c808cbac4a5e8
King Z <kingz@relationshipforum.org>
2014-06-24 16:03:37 -0500

    Body class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
   def avatar(user = current_user)
     image_tag(user.avatar_url, class: "avatar", height: 40, width: 40)
   end
+
+  def body_class
+    kls = []
+    kls << "theme-default"
+    kls << "no-main-menu" unless content_for?(:menu)
+    kls << "page-mail" if controller_name == "messages"
+    kls << yield(:body_class) if content_for?(:body_class)
+
+    kls.join(" ")
+  end
 end
