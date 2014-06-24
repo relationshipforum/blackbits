b8f8d2533d7e58eea5649d751e412af70338b4ea
King Z <kingz@relationshipforum.org>
2014-06-24 10:41:52 -0500

    Redis is localhost for sure, now

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,9 +1,4 @@
-if Rails.env == "production"
-  REDIS_URL = "redis://:herpityderpityderp!@cnmrelo.lyceed.com:6379/0/bb"
-else
-  REDIS_URL = "redis://127.0.0.1:6379/0/bb"
-end
-
+REDIS_URL = "redis://127.0.0.1:6379/0/bb"
 REDIS = Redis.new(url: REDIS_URL)
 
 Blackbits::Application.config.cache_store = :redis_store, REDIS_URL
