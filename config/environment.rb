1dd677cd26d2530323213a912409c0b063de346b
King Z <kingz@relationshipforum.org>
2014-08-11 16:19:18 -0500

    Sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :user_name      => SENDGRID[:username]
+  :password       => SENDGRID[:password],
+  :domain         => "relationshipforum.org",
   :enable_starttls_auto => true
 }
---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,5 @@
+SENDGRID = {
+  username: "",
+  password: "",
+  domain: ""
+}
