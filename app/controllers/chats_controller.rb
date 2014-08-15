226b7beab7ded989dfa9c1deda6f9825e183eab4
King Z <kingz@relationshipforum.org>
2014-08-15 14:51:07 +0200

    Derp. Only mark as read if the chat is private

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -13,7 +13,7 @@ class ChatsController < ApplicationController
               hash = JSON.parse(message)
               hash["message"] = Rinku.auto_link(hash["message"], :all, 'target="_blank"')
               hash[:timestamp] = Time.now.iso8601
-              Conversation.find(conversation_id).read_by!(current_user)
+              Conversation.find(conversation_id).read_by!(current_user) unless conversation_id == "global"
               tubesock.send_data(JSON(hash))
             end
           end
